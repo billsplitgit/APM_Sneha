1362b5b920f2b2a033b885335a8a1d7cfe9b1d0e
billsplitgit <billsplitgit@gmail.com>
2017-08-13 19:58:32 +0530

    version1

--- APM-Start-Updated/src/app/app.component.ts
+++ APM-Start-Updated/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <h1>Product list</h1>
     `
 })
 export class AppComponent { }
